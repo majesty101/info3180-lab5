9e698b2322702e16dfd0c0cf246a4abc73af9111
majesty101 <jody.harrison32@gmail.com>
2021-03-19 09:50:36 -0500

    Edited

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template, request, redirect, url_for, flash
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
@@ -11,6 +12,16 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+    return wrap
+
 
 app.config.from_object(Config)
 from app import views
